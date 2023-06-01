dafea27473e13269211543799e5f6338e466eeb3
Elemento <mittalvishesh021@gmail.com>
2023-06-01 13:07:09 +0530

    Modified the JS file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("isomorphic-unfetch");
+const fs_wopro = require("fs");
 const { promises: fs } = require("fs");
+const { parse } = require("csv-parse");
+const CsvReadableStream = require("csv-reader")
 const path = require("path");
 
 async function main() {
@@ -7,14 +10,36 @@ async function main() {
     await fs.readFile(path.join(process.cwd(), "./README.template.md"))
   ).toString("utf-8");
 
-  const office_quote = await (
-    await fetch("https://officeapi.dev/api/quotes/random")
-  ).json();
-  console.log(office_quote);
+  // const office_quote = await (
+  //   await fetch("https://officeapi.dev/api/quotes/random")
+  // ).json();
+
+  // Loading the CSV file
+  const data = [];
+
+  fs_wopro.createReadStream("quotes_small.csv", "utf-8")
+    .pipe(
+      new CsvReadableStream({
+        skiplines: 2,
+        delimiter: ";",
+        parseNumbers: true,
+        parseBooleans: true,
+      })
+    )
+    .on("data", function (row) {
+      row.pop()
+      data.push(row)
+    })
+    .on("end", function () {
+      console.log("No more rows!");
+    });
+
+  let index = Math.floor(Math.random() * data.length)
+  quote = data[index]
 
   const readme = readmeTemplate
-    .replace("{office_quote}", office_quote.data.content)
-    .replace("{office_character}", `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`)
+    .replace("{office_quote}", quote[0])
+    .replace("{office_character}", quote[1])
 
   await fs.writeFile("README.md", readme);
 }
